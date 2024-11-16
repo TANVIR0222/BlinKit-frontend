8e822b63f91db99af74ba2b4e1124b2d418c03c5
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-16 23:39:28 +0600

    add React toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from "react";
+import toast, { Toaster } from 'react-hot-toast';
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/Router";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <StrictMode>
       <RouterProvider router={router} />
-    </StrictMode>
+      <Toaster />
   </Provider>
 );
