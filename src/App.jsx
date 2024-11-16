c510f0c69a1b1efb9cb7714d0e12a186538be87c
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-16 17:57:26 +0600

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Outlet } from "react-router-dom"
+import Header from "./shared/Header"
+import Footer from "./shared/Footer"
 
-import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
-    hello server
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
