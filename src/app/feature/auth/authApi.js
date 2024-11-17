286417d063c84363cdee579c18e9b2d3cc96faff
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-17 16:46:41 +0600

    add logout

--- src/app/feature/auth/authApi.js
+++ src/app/feature/auth/authApi.js
@@ -1,48 +1,57 @@
-import baseUrl from '@/utils/baseUrl'
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import baseUrl from "@/utils/baseUrl";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const authApi = createApi({
-  reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: `${baseUrl()}/user/`
-  }),
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl: `${baseUrl()}/user/` }),
   endpoints: (builder) => ({
     userRegister: builder.mutation({
       query: (newUser) => ({
-        url:'register',
-        method:'POST',
-        body: newUser
-      })
+        url: "register",
+        method: "POST",
+        body: newUser,
+      }),
     }),
     userLogin: builder.mutation({
       query: (login) => ({
-        url:'login',
-        method:'POST',
-        body: login
-      })
+        url: "login",
+        method: "POST",
+        body: login,
+      }),
     }),
     userForgotPassword: builder.mutation({
       query: (forgotPassword) => ({
-        url:'forgot-password',
-        method:'PUT',
-        body: forgotPassword
-      })
+        url: "forgot-password",
+        method: "PUT",
+        body: forgotPassword,
+      }),
     }),
     userForgotOTPVerify: builder.mutation({
       query: (forgotOTP) => ({
-        url:'verify-forgoot-password-otp',
-        method:'PUT',
-        body: forgotOTP
-      })
+        url: "verify-forgoot-password-otp",
+        method: "PUT",
+        body: forgotOTP,
+      }),
     }),
     userResetPassword: builder.mutation({
       query: (resetPass) => ({
-        url:'rest-password',
-        method:'PUT',
-        body: resetPass
-      })
+        url: "rest-password",
+        method: "PUT",
+        body: resetPass,
+      }),
+    }),
+    userLogout: builder.mutation({
+      query: () => `logout`,
     }),
   }),
-})
+});
 
-export const { useUserRegisterMutation , useUserLoginMutation , useUserForgotPasswordMutation , useUserForgotOTPVerifyMutation , useUserResetPasswordMutation} = authApi
+export const {
+  useUserRegisterMutation,
+  useUserLoginMutation,
+  useUserForgotPasswordMutation,
+  useUserForgotOTPVerifyMutation,
+  useUserResetPasswordMutation,
+  useUserLogoutMutation,
+} = authApi;
