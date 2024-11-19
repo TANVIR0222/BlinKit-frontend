85f88823de9714b42890064cc24c83cc1924acef
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-19 23:50:30 +0600

    add tagTypes

--- src/app/feature/category/categoryApi.js
+++ src/app/feature/category/categoryApi.js
@@ -4,6 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const categoryApi = createApi({
   reducerPath: 'categoryApi',
   baseQuery: fetchBaseQuery({ baseUrl: `${baseUrl()}/category/` }),
+  tagTypes: ['Category'],
   endpoints: (builder) => ({
     addCategory: builder.mutation({
         query: (newPost) => ({
@@ -11,9 +12,14 @@ export const categoryApi = createApi({
             method: 'POST',
             body: newPost,
         }),
-    })
+        invalidatesTags: ['Category'],
+    }),
+    getAllCategory: builder.query({
+        query: () => `all-category`,
+        providesTags: ['Category'],
+    }),
   }),
 })
 
 
-export const { useAddCategoryMutation } = categoryApi
+export const { useAddCategoryMutation , useGetAllCategoryQuery } = categoryApi
