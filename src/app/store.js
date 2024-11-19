ddfe1cff202d6d0805f73ac7774470155bbcde35
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-19 18:11:24 +0600

    add Category categoryApi

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { authApi } from './feature/auth/authApi'
 import  authReducer  from './feature/auth/authSlice'
+import { categoryApi } from './feature/category/categoryApi'
 
 export const store = configureStore({
   reducer: {
     auth:  authReducer,
     [authApi.reducerPath]: authApi.reducer,
+    [categoryApi.reducerPath]: categoryApi.reducer,
   },
   
   middleware: (getDefaultMiddleware) =>
