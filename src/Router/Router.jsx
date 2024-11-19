e3bb0f753e00fd09714c0acffca50a237695ed99
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-19 22:45:09 +0600

    router

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,13 +1,91 @@
 
+import App from '@/App';
+import Dashboard from '@/Layout/Dashboard';
+import Category from '@/page/adminPage/Category';
+import Product from '@/page/adminPage/Product';
+import SubCategory from '@/page/adminPage/SubCategory';
+import UploadeProduct from '@/page/adminPage/UploadeProduct';
+import ForgotPassword from '@/page/auth/ForgotPassword';
+import Login from '@/page/auth/Login';
+import OTPverify from '@/page/auth/OTPVerify';
+import Profile from '@/page/auth/Profile';
+import Register from '@/page/auth/Register';
+import ResetPassword from '@/page/auth/ResetPassword';
+import HomePage from '@/page/Home/HomePage';
+import Address from '@/page/myInformations/Address';
+import MyOrder from '@/page/myInformations/MyOrder';
+import SearchPage from '@/page/search/SearchPage';
 import {createBrowserRouter} from 'react-router-dom'
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <App />,
+      children:[
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/search",
+          element: <SearchPage />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        {
+          path: "/forgot-password",
+          element: <ForgotPassword />,
+        },
+        {
+          path: "/verification-opt",
+          element: <OTPverify />,
+        },
+        {
+          path: "/resrt-password",
+          element: <ResetPassword />,
+        },
+        
+        {
+          path: "dashboard",
+          element: <Dashboard />,
+          children:[
+            {
+              path: "profile",
+              element: <Profile />,
+            },
+            {
+              path: "order",
+              element: <MyOrder />,
+            },
+            {
+              path: "address",
+              element: <Address />,
+            },
+            {
+              path: "category",
+              element: <Category />,
+            },
+            {
+              path: "sub-category",
+              element: <SubCategory />,
+            },
+            {
+              path: "uploade-product",
+              element: <UploadeProduct />,
+            },
+            {
+              path: "product",
+              element: <Product />,
+            },
+          ]
+        },
+        
+      ]
     },
-  ],{
-    future: {
-      v7_normalizeFormMethod: true,
-    },
-  });
+  ]);
