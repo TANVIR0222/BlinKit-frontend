49866333df7445b3f6631fd006d123dfac347db4
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-19 22:38:25 +0600

    image uploadeApi

---
+++ src/app/feature/uploadeImage/imageApi.js
@@ -0,0 +1,20 @@
+import baseUrl from '@/utils/baseUrl'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const imageApi = createApi({
+  reducerPath: 'imageApi',
+  baseQuery: fetchBaseQuery({ baseUrl: `${baseUrl()}/file` }),
+  endpoints: (builder) => ({
+    imageUploade: builder.mutation({
+        query: ({id,formData}) => ({
+            url: `uploade/${id}`,
+            method: 'POST',
+            body: formData,
+        }),
+    })
+  }),
+})
+
+
+export const { useImageUploadeMutation } = imageApi
