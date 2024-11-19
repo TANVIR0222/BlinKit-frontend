5a0ffe3a1a5a0838eb4c8bec2d8cf152c04c4dc3
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-19 22:40:45 +0600

    Dashboard Layout

---
+++ src/Layout/Dashboard.jsx
@@ -0,0 +1,26 @@
+import Sidebar from "@/components/common/Sidebar";
+import UserMenu from "@/components/common/UserMenu";
+import { GoLinkExternal } from "react-icons/go";
+import { useSelector } from "react-redux";
+import { Link, Outlet } from "react-router-dom";
+
+const Dashboard = () => {
+  const { user } = useSelector((state) => state.auth);
+  return (
+    <section className="bg-white">
+      <div className="container mx-auto p-3 grid lg:grid-cols-[250px,1fr]  ">
+        {/**left for menu */}
+        <div className="py-4 sticky top-24 max-h-[calc(100vh-96px)] overflow-y-auto hidden lg:block border-r ">
+          <Sidebar />
+        </div>
+
+        {/**right for content */}
+        <div className="bg-white min-h-[75vh] ">
+          <Outlet />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Dashboard;
