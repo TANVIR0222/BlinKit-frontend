f17a8e6e73e2777dc77f57e450f012776957ea7a
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-20 22:48:51 +0600

    category page

---
+++ src/page/adminPage/Category.jsx
@@ -0,0 +1,56 @@
+import { useGetAllCategoryQuery } from "@/app/feature/category/categoryApi";
+import UploadeCategoryModel from "./UploadeCategoryModel";
+import Loading from "@/components/common/Loading";
+import NoData from "@/components/common/NoData";
+import EditeCategory from "./EditeCategory ";
+import DeleteProduct from "@/components/common/DeleteProduct";
+
+
+const Category = () => {
+  const { data: allCategory, isLoading } = useGetAllCategoryQuery();
+
+
+  return (
+    <div>
+      <div className="p-2   bg-white shadow-md flex items-center justify-between">
+        <h2 className="font-semibold">Category</h2>
+        <button className="">
+          <UploadeCategoryModel />
+        </button>
+      </div>
+      {allCategory === undefined && <NoData />}
+
+      <div className="p-4 grid  grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2">
+        {isLoading ? (
+          <div className="flex justify-center items-center h-screen">
+            <Loading />
+          </div>
+        ) : (
+          allCategory?.categorys.map((category) => (
+            <div className="w-32 h-60 rounded shadow-md" key={category._id}>
+              <img
+                alt={category.name}
+                src={category.image}
+                className="w-full object-scale-down"
+              />
+              <h2 className="text-center h-16">{category.name}</h2>
+              <div className="items-center h-9 flex gap-2">
+                <button className="flex-1 font-medium py-1 rounded">
+                  <EditeCategory category={category} />
+                </button>
+                <button
+                  className="flex-1 bg-red-100 hover:bg-red-200 text-red-600 font-medium py-1 rounded"
+                >
+                  {/* common conponent */}
+                  <DeleteProduct id={category._id} />
+                </button>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
