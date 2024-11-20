79944ced0dc1ade1844f829e3ef9bd499fbb9f6a
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-20 22:48:34 +0600

    category delete product

---
+++ src/components/common/DeleteProduct.jsx
@@ -0,0 +1,48 @@
+import { useDeleteCategoryMutation } from "@/app/feature/category/categoryApi";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+const DeleteProduct = ({ id }) => {
+  const [deleteCategory] = useDeleteCategoryMutation();
+
+  const handleDelete = async (deleteId) => {
+    try {
+       await deleteCategory(deleteId).unwrap();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <AlertDialog>
+        <AlertDialogTrigger>Delete</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={() => handleDelete(id)}>
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default DeleteProduct;
