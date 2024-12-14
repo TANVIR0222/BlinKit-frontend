1890286586126e689af1d8d75786f67f21a40eb1
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-14 22:47:55 +0600

    add Live link

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,7 @@
 
 const baseUrl = () => {
-    return 'http://localhost:8080/api/v1'
+    // return 'http://localhost:8080/api/v1'
+    return 'https://blinkit-client.onrender.com/api/v1'
 };
 
 export default baseUrl;
