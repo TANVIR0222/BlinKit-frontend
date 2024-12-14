6699fb56e561fc89e46c05ba7635092d504174f7
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-14 20:33:30 +0600

    add Add To Cart Button

--- src/components/common/ProductDisplayPage.jsx
+++ src/components/common/ProductDisplayPage.jsx
@@ -77,9 +77,6 @@ const ProductDisplayPage = () => {
               <p className="text-lg text-red-500 my-2">Out of Stock</p>
             ) : (
               <>
-                <button className="my-4 px-4 py-1 bg-green-600 hover:bg-green-700 text-white rounded">
-                  Add
-                </button>
                 <div className="my-4">
                   <AddToCartButton data={data} />
                 </div>
