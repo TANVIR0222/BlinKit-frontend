fcb5467563153e583fe0ceaf3f75ad6fef8dbab5
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-14 22:42:13 +0600

    Search page update

--- src/components/common/Search.jsx
+++ src/components/common/Search.jsx
@@ -29,7 +29,7 @@ const Search = () => {
     <div className='w-full  min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200 '>
         <div>
             {
-                (isMobile && isSearchPage ) ? (
+                (isSearchPage ) ? (
                     <Link to={"/"} className='flex justify-center items-center h-full p-2 m-1 group-focus-within:text-primary-200 bg-white rounded-full shadow-md'>
                         <FaArrowLeft size={20}/>
                     </Link>
