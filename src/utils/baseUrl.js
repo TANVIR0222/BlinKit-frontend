553d1ff84e392d066dbdfe46c44f1cca2da05590
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-15 18:09:35 +0600

    live link

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,7 +1,7 @@
 
 const baseUrl = () => {
-    // return 'https://blinkit-client.onrender.com/api/v1'
-    return 'http://localhost:8080/api/v1'
+    return 'https://blinkit-client.onrender.com/api/v1'
+    // return 'http://localhost:8080/api/v1'
 
 };
 
