ae02b53474d8f700e999961acfc9b1bfbaad48c5
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-16 15:45:45 +0600

    update user cart

--- src/Hooks/useCartTotal.jsx
+++ src/Hooks/useCartTotal.jsx
@@ -1,35 +1,24 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useGetSingleUserCartQuery } from "@/app/feature/cart/cartApi";
 import { handleAddItemCart } from "@/app/feature/cart/cartSlice";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import useUser from "./useUser";
 
 const useCartTotal = () => {
-  const [ user ] = useUser()
-  console.log(user?._id);
-
+  const [user] = useUser();
   const id = user?._id;
-  
 
-  const { data: cartItem , isFetching } = useGetSingleUserCartQuery(id, { skip: !id});
-  console.log(cartItem);
+  const { data: cartItem } = useGetSingleUserCartQuery(id, { skip: !id });
 
   const dispatch = useDispatch();
 
-  dispatch(handleAddItemCart(cartItem?.data));
-  const totalQty = cartItem?.data?.reduce(
-    (acc, item) => acc + item.quantity,
-    0
-  );
-  // Calculate total price
-  // const totalPrice = cartItem?.data.reduce((acc, item) => {
-  //   const price = item.productId.price;
-  //   const discount = item.productId.discount;
-  //   const quantity = item.quantity;
+  useEffect(() => {
+    if (cartItem?.data) {
+      dispatch(handleAddItemCart(cartItem.data)); // Dispatch after render
+    }
+  }, [cartItem?.data, dispatch]);
 
-  //   const discountedPrice = price + discount; // Discount is negative, so we add it
-  //   return acc + discountedPrice * quantity;
-  // }, 0);
+  const totalQty = cartItem?.data?.reduce((acc, item) => acc + item.quantity, 0) || 0;
 
   let currentPrice = 0; // Total without discount
   let totalDiscountedPrice = 0; // Total after applying discount
@@ -45,13 +34,9 @@ const useCartTotal = () => {
     totalDiscountedPrice += discountedPrice * quantity;
   });
 
+  const totalPrice = totalDiscountedPrice;
 
-// console.log("Total Price (without discount): ₹", currentPrice);
-// console.log("Total Price (with discount): ₹",totalDiscountedPrice);
-
-const totalPrice = totalDiscountedPrice;
- 
-  return [totalQty , totalPrice ,cartItem ];
+  return [totalQty, totalPrice, cartItem];
 };
 
 export default useCartTotal;
